eb444406ba18f2d2e3477cdcb5bb5f4ec398ff7f
Adhish-hub <adish.tiwari@startappss.com>
2026-04-28 17:24:27 +0530

    initial commit

---
+++ schoolw3 js/script.js
@@ -0,0 +1,33 @@
+let x = 9;
+let y = 9;
+console.log(x*y);
+
+document.getElementById("main").innerHTML = "jcsjbcjswc jsw";
+
+let a = "John" + " " + "Doe";
+console.log(a);
+
+let b = "5" + 2 + 3;
+console.log(b);
+
+let s = "adhish"
+let v = 464;
+let t = (s+v);
+console.log(t);
+
+let q = 10;
+q += 5;
+console.log(q);
+
+
+let w = 3
+e = w ** 4;
+console.log(e);
+ 
+let r = 9;
+t = Math.pow(r,2)
+console.log(t);
+
+i = 664;
+i -= 100;
+console.log(i);
