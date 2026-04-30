8f38e162a514174d335d9dbbb644495caaab0f43
Adhish-hub <adish.tiwari@startappss.com>
2026-04-30 17:43:07 +0530

    JS Functions, Objects, Scope and Dates

--- schoolw3 js/script.js
+++ schoolw3 js/script.js
@@ -23,7 +23,7 @@
 // let w = 3
 // e = w ** 4;
 // console.log(e);
- 
+
 // let r = 9;
 // t = Math.pow(r,2)
 // console.log(t);
@@ -84,11 +84,11 @@ If the first value is false, the second value is assigned.*/
 // let age = 16;
 // if (age>=18){
 //     console.log('you can drive');
-    
+
 // }
 // else{
 //     console.log('you can not drive');
-    
+
 // }
 
 // let age = 16;
@@ -96,37 +96,37 @@ If the first value is false, the second value is assigned.*/
 // if (country == "India"){
 //     if(age >= 18){
 //         console.log('you can drive');
-        
+
 //     }else{
 //     console.log('you can not drive');
-    
+
 // }
 // }
 
 // if (country == "India" && age >=18){
 //     console.log('you can drive');
-    
+
 // }else{
 //     console.log('you can not drive');
-    
+
 // }
 
 // let time = 65
 // if (time < 12){
 //     console.log('good morning');
-    
+
 // } else if (time>=12 && time<17){
 //     console.log('good afternoon');
-    
+
 // }else if (time>=17 && time < 20){
 //     console.log('good evening');
-    
+
 // }else if  (time >= 20 && time <25){
 //     console.log('good night');
-    
+
 // }else{
 //     console.log('invalid time');
-    
+
 // }
 
 
@@ -207,8 +207,129 @@ If the first value is false, the second value is assigned.*/
 // let z = x / y;
 // console.log(z);
 
-let x = 6;
-x.MAX_VALUE
-console.log(x);
+// let x = 6;
+// x.MAX_VALUE
+// console.log(x);
+
+function sayHello() {
+    return "Hello World";
+}
+
+let message = sayHello();
+// console.log(message);
+document.getElementById("main").innerHTML = sayHello();
 
 
+
+// Parameters
+// function multiply(a, b){
+//     return a * b;
+// }
+
+// let result = multiply(10, 45);
+// console.log(result);
+
+function toCelsius(fahrenheit) {
+    return (5 / 9) * (fahrenheit - 32);
+}
+
+let value = toCelsius(77);
+console.log(value);
+
+
+
+function multiply(a, b) {
+    return "Done";
+
+    return a * b;
+}
+
+let result = multiply(4, 3);
+console.log(result);
+/*value will be "Done" because the function will return "Done" and exit before it reaches the second return statement. The second return statement will never be executed. All the values or operations after the first return statement will be executed. */
+
+
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// let result = multiply(4, 5);
+// In the example above:
+
+// a and b are parameters
+// 4 and 5 are arguments
+// The argument 4 is assigned to the parameter a.
+
+// The argument 5 is assigned to the parameter b.
+
+// Arguments can be variables they do not need to  be number
+// let x = 10;
+// let y = 20;
+// function mul(a, b){
+//     return a*b;
+// }
+// let res = mul(x, y);
+// console.log(res);
+
+// const add = (a, b = 2) => a + b;
+// let res1 = add(5);
+// console.log(res1);
+// // You can now set a default value for a parameter.
+// // The default value is used if no argument is provided.
+
+// const h1 = () => "Hello World";
+// console.log(h1());
+
+// Objects
+
+// let info = {
+//     firstName: "Adhish",
+//     lastName: "Tiwari",
+//     age: 23,
+//     city: "Dewas",
+//     fullName : function(){
+//         return (this.firstName + " " + this.lastName).toLowerCase();
+//     }
+// }
+// console.log(info.fullName());
+
+
+// const p1 = {
+//     name: "Adhish Tiwari" ,
+//     hello: function(){
+//         return "hello " + this.name;
+//         }
+// }
+// console.log(p1.hello());
+
+
+
+// let info = {
+//     firstName: "Adhish",
+//     lastName: "Tiwari",
+//     age: 23,
+//     city: "Dewas"
+// }
+
+// let x = " ";
+// for (let a in info){
+//     x += info[a] + " ";
+// }
+// document.getElementById("main").innerHTML = x;
+
+const fruits = { Bananas: 300, Oranges: 200, Apples: 500 };
+
+let text = "";
+for (let [fruit, value] of Object.entries(fruits)) {
+    text += fruit + ": " + value + "<br>";
+}
+document.getElementById("main").innerHTML = text;
+
+const d = new Date();
+d.toDateString();
+d.getHours();
+console.log(d);
+
+
+getTime()
